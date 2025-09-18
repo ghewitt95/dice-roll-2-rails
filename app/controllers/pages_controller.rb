6f1c1698566194a91b5c9f1b334ef3b26f2adaff
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-09-18 00:11:19 +0000

    created home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def home
+
+    render ({ template: "dice/home"})
+  end
+
+  def
+
+    render ({ template: "dice/home"})
+  end
+
+  def
+
+    render ({ template: "dice/home"})
+  end
+
+  def
+
+    render ({ template: "dice/home"})
+  end
+
+  def
+    render ({ template: "dice/home"})
+  end
+end
